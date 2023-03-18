84dc4e79b463fdb0cf0b007653fbbae08f473f15
FredrickUdom <fredrickudom@gmail.com>
2023-03-18 09:13:43 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#Include <stdio.h>
+#include <stdio.h>
 
-int main(0){
-	printf("Programming is like building a multilingual puzzle");
+int main(void){
+	puts("Programming is like building a multilingual puzzle");
 		return (0);
 }
